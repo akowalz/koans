d2584894ae708841b82f6d49c9cc80b010c52111
Alex Kowalczuk <askowalczuk93@gmail.com>
2014-03-14 16:42:54 -0500

    initial commit

---
+++ triangle.rb
@@ -0,0 +1,39 @@
+# Triangle Project Code.
+
+# Triangle analyzes the lengths of the sides of a triangle
+# (represented by a, b and c) and returns the type of triangle.
+#
+# It returns:
+#   :equilateral  if all sides are equal
+#   :isosceles    if exactly 2 sides are equal
+#   :scalene      if no sides are equal
+#
+# The tests for this method can be found in
+#   about_triangle_project.rb
+# and
+#   about_triangle_project_2.rb
+#
+
+def triangle(a, b, c)
+  sides = [a,b,c]
+  biggest_side = sides.max
+  other_sides = sides.select { |x| x != biggest_side } 
+  
+
+  if sides.bsearch { |x| x <= 0 }
+    raise TriangleError
+  elsif ((a>=(b+c)) || ( b>=(a+c) ) || (c>=(a+b)))
+    raise TriangleError
+  end
+  if a == b and b == c 
+    return :equilateral
+  elsif a != b and a != c and b != c
+    return :scalene
+  else 
+    return :isosceles
+  end
+end
+
+# Error class used in part 2.  No need to change this code.
+class TriangleError < StandardError
+end
